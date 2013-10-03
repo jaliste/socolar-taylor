39ea0b9725741d6474d14a2fc046a77958d534ac
José Aliste <jaliste@src.gnome.org>
2013-10-03 00:08:48 -0300

    Nueva version. refactored.

--- first_neighboors.py
+++ first_neighboors.py
@@ -27,39 +27,45 @@ Center=til[V[1][1]]
 
 #til[V[1][1]]
 #Diferencia causada por los missed match
-Diff=0
+dE = 0
+
+# Esta funcion entrega, dado el label de un hexagono
+# la codificacion del hexagono como string
+# ademas permite rotar el hexago por un multiplo
+# n de n_alpha
+def tile (label, n_alpha):
+	tile = til[label]
+	return tile[n_alpha:] + tile[0:n_alpha]
+
+def arista_dE (R, C):
+	return 1 if R[3] != C[0] else 0
 
 for i in [-1, 0, 1]:
     for j in [-1 ,0, 1]:
-        if (i+1!=j+1):
-            if (i+1!=1 or j+1!=2):
-               if (i+1==0) and (j+1==2):
-                   R=(til[V[0][2]])[5:6]+(til[V[0][2]][0:5])
-                   C=(til[V[1][1]])[5:6]+(til[V[1][1]][0:5])
-                   if R[3]!=C[0]:
-                       Diff=Diff+1
-               elif (i+1==0) and (j+1==1):
-                    R=(til[V[0][1]])[4:6]+(til[V[0][1]][0:4])
-                    C=(til[V[1][1]])[4:6]+(til[V[1][1]][0:4])
-                    if R[3]!=C[0]:
-                       Diff=Diff+1
-               elif (i+1==1) and (j+1==0):
-                    R=(til[V[1][0]])[3:6]+(til[V[1][0]][0:3])
-                    C=(til[V[1][1]])[3:6]+(til[V[1][1]][0:3])
-                    if R[3]!=C[0]:
-                       Diff=Diff+1
-               elif (i+1==2) and (j+1==0):
-                    R=(til[V[2][0]])[2:6]+(til[V[2][0]][0:2])
-                    C=(til[V[1][1]])[2:6]+(til[V[1][1]][0:2])
-                    if R[3]!=C[0]:
-                       Diff=Diff+1
-               elif (i+1==2) and (j+1==1):
-                    R=(til[V[2][1]])[1:6]+(til[V[2][1]][0:1])
-                    C=(til[V[1][1]])[1:6]+(til[V[1][1]][0:1])
-                    if R[3]!=C[0]:
-                       Diff=Diff+1
-            else:
-                if (til[V[1][2]])[3]!=Center[0]:
-                    Diff=Diff+1
-                 
+        if (i+1 != j+1):
+            if (i+1 != 1 or j+1 != 2):
+               if (i+1 == 0) and (j+1 == 2):
+                   R = tile([V[0][2]],5)
+                   C = tile([V[1][1]],5)
+                   dE += arista_dE (R,C)
+               elif (i+1 == 0) and (j+1 == 1):
+                    R = tile([V[0][1]],4)
+                    C = tile([V[1][1]],4)
+                    dE += arista_dE (R,C)
+               elif (i+1 == 1) and (j+1 == 0):
+                    R = tile([V[1][0]],3)
+                    C = tile([V[1][1]],3)
+                    dE += arista_dE (R,C)
+              elif (i+1 == 2) and (j+1 == 0):
+                    R = tile([V[2][0]],2)
+                    C = tile([V[1][1]],2)
+                    dE += arista_dE (R,C)
+              elif (i+1 == 2) and (j+1 == 1):
+                    R = tile([V[2][1]],1)
+                    C = tile([V[1][1]],1)
+                    dE += arista_dE (R,C)
+           else:
+				R = til[V[1][2]]
+				C = Center
+                dE += arista_dE (R,C) 
         
